65bb1279e8d28e96729143a8e40f13f102d49c17
las-r <128936184+las-r@users.noreply.github.com>
2026-09-15 19:22:20 -0400

    Make space a no-op

--- tact/interpreter.py
+++ tact/interpreter.py
@@ -24,6 +24,7 @@ def run(code: str) -> None:
             case "<": i += 0 if stk.pop() < 0 else 1
             case "i": stk.append(int(input("Value input: ")))
             case "p": print(stk[-1])
+            case " ": pass
             case _: raise Exception(f"Unknown character: {char}")
         
         #print(f"{i} ({char}), {stk}")
